474505039512ae9eb706d6bf3bdb6f7b29bcc029
Cesar De la Torre <cesardl@microsoft.com>
2016-06-28 23:27:14 -0700

    .NET Core Cosole app targeting multiple frameworks (.NET Core 1.0 and .NET Framework 4.5.2)

--- src/NetCoreConsole.MultipleFXs/Program.cs
+++ src/NetCoreConsole.MultipleFXs/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+#if NET452
+using System.Diagnostics; 
+#endif
 
 namespace NetCoreConsole.MultipleFXs
 {
@@ -9,6 +10,26 @@ namespace NetCoreConsole.MultipleFXs
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Howdy!");
+            Console.WriteLine("This line is executed no matter what framework I'm running my Core app on");
+
+#if NET452
+            // Access something that requires the traditional .NET Framework, like some Windows related stuff
+            
+            // Writing to the Windows EventLog
+            // NOTE: Writing to the Windows EventLog might need ADMIN PRIVILEGES!!
+            string sSource = "My .NET Core Console App";
+            string sLog = "Application";
+            string sEvent = "My sample Event when running on the .NET Framework";
+
+            if (!EventLog.SourceExists(sSource))
+	            EventLog.CreateEventSource(sSource,sLog);
+
+            EventLog.WriteEntry(sSource,sEvent);
+            EventLog.WriteEntry(sSource, sEvent, EventLogEntryType.Warning,  234);
+            Console.WriteLine("EventLog code was executed because it was running on top of .NET Framework 4.5.2");
+#endif
+            Console.ReadKey();
         }
     }
 }
